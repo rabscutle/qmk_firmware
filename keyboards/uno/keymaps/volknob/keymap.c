f9b22f9d8b67a94196d5d989525e944758cf3b66
Dan Wright <dan.wright@everactive.com>
2021-01-11 18:07:24 -0500

    Enable media keys (so volume actually works) and make knob click send zoom and google meet mute key

--- keyboards/uno/keymaps/volknob/keymap.c
+++ keyboards/uno/keymaps/volknob/keymap.c
@@ -15,29 +15,22 @@ uint8_t ledIndex = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_F10
+        UNO
     )
 };
 
-/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
 		case UNO:
             if (record->event.pressed) {
-                pressTimer = timer_read();
             } else {
-                uint16_t timeElapsed = timer_elapsed(pressTimer);
-                if (timeElapsed < CUSTOM_LONGPRESS) {
-                    send_string(pasta[pastaIndex]);
-                } else {
-                    reset_keyboard();
-                }
+                SEND_STRING(SS_LALT("a"));
+                SEND_STRING(SS_LCTL("d"));
             }
         break;
     }
     return false;
 }
-*/
 
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
