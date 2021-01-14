e9b5d0b61f520aad82c05bf5d5d8fe78a9b625ce
Dan Wright <dan.wright@everactive.com>
2021-01-14 11:13:03 -0500

    Add cmd-4 shortcut to raise layer for mission control

--- keyboards/lily58/keymaps/dtwright/keymap.c
+++ keyboards/lily58/keymaps/dtwright/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
   KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                    KC_LALT, KC_BSPC, MO(_LOWER), LT(_ARROWS, KC_SPC), LT(_MOUSE, KC_SPC), MO(_RAISE), KC_RGUI, KC_RCTL \
+                    KC_LALT, KC_LGUI, MO(_LOWER), LT(_ARROWS, KC_SPC), LT(_MOUSE, KC_SPC), MO(_RAISE), KC_RGUI, KC_RCTL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, XXXXXXX, \
   KC_CAPS, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_F12,  XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, KC_BSLS, \
-                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
+                             _______, LGUI(KC_4), _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
