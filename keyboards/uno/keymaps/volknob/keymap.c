1dab7ea5b0d4914f736197cf0f8494871babf883
Dan Wright <dan.wright@everactive.com>
2021-01-15 19:40:20 -0500

    Fully working keymap with volume and switchable scroll direction

--- keyboards/uno/keymaps/volknob/keymap.c
+++ keyboards/uno/keymaps/volknob/keymap.c
@@ -1,17 +1,44 @@
+/*
+This is a keymap for doing some volume and scrolling stuff 
+with the Uno v2. Started with the copypasta keymap
+by u/Snipeye and got out of hand. :)
+
+Copyright 2021 Dan Wright <dtwright@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 #define RGB_STEPS 16
 #define LED_INDEX_START 8
 // how long allowed between double-taps to switch scroll direction?
-#define SCROLL_HOLD_TIME 1500
+#define SCROLL_HOLD_TIME 300 
+
+// start colors for each mode
+#define VOL_COLOR 128
+#define V_SCROLL_COLOR 192
+#define H_SCROLL_COLOR 32
 
 enum uno_keycode
 {
-  UNO = SAFE_RANGE
+    MODE = SAFE_RANGE
 };
 
 enum encoder_names {
-	_ENCODER,
+    _ENCODER,
 };
 
 uint16_t scrollTimer = 0;
@@ -24,35 +51,32 @@ uint8_t ledIndex = LED_INDEX_START;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        UNO
+        MODE
     )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	switch (keycode) {
-	case UNO:
-            if (record->event.pressed) {
+    switch (keycode) {
+    case MODE:
+        if (record->event.pressed) {
+        } else {
+            if(scrollMode) {
+                dprint("in scroll mode\n");
+                dprintf("%d %d\n", scrollTimer, scrollClicks);
+                scrollClicks++;
+                if(scrollTimer == 0) scrollTimer = timer_read();
             } else {
-                if(scrollMode) {
-                    scrollClicks++;
-                    if(scrollTimer == 0) scrollTimer = timer_read();
-                    if(timer_elapsed(scrollTimer) < SCROLL_HOLD_TIME && scrollClicks > 1) {
-                        if(scrollHoriz) {
-                            scrollHoriz = false;
-                        } else {
-                            scrollHoriz = true;
-                        }
-                        scrollClicks = 0;
-                    } else if(timer_elapsed(scrollTimer) > SCROLL_HOLD_TIME) {
-                        scrollMode = 0;
-                    }
-                    scrollTimer = timer_read();
-                } else {
-                    scrollMode = 1;
-                    scrollClicks = 0;
-                    scrollTimer = 0;
-                }
+                dprint("entering scroll mode\n");
+                rgblight_sethsv_noeeprom(
+                        scrollHoriz ? H_SCROLL_COLOR : V_SCROLL_COLOR, 
+                        255, 
+                        255
+                );
+                scrollMode = 1;
+                scrollClicks = 0;
+                scrollTimer = 0;
             }
+        }
         break;
     }
     return false;
@@ -61,15 +85,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
     rgblight_mode_noeeprom(1);
-    rgblight_sethsv_noeeprom((255/RGB_STEPS)*ledIndex, 255, 255);
-    //rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD);
+    rgblight_sethsv_noeeprom(VOL_COLOR, 255, 255);
+
+    // set to true to get hid console output from various print statements
+    debug_enable = false;
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == _ENCODER) { /* First encoder */
         if (clockwise) {
-            ledIndex++;
-            ledIndex %= RGB_STEPS;
+            // ledIndex++;
+            // ledIndex %= RGB_STEPS;
             if(!scrollMode) {
                 register_code(KC_VOLU);
                 unregister_code(KC_VOLU);
@@ -83,9 +109,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
                 }
             }
         } else {
-            ledIndex += RGB_STEPS;
-            ledIndex--;
-            ledIndex %= RGB_STEPS;
+            // ledIndex += RGB_STEPS;
+            // ledIndex--;
+            // ledIndex %= RGB_STEPS;
             if(!scrollMode) {
                 register_code(KC_VOLD);
                 unregister_code(KC_VOLD);
@@ -99,8 +125,29 @@ void encoder_update_user(uint8_t index, bool clockwise) {
                 }
             }
         }
-        rgblight_sethsv_noeeprom((255/RGB_STEPS)*ledIndex, 255, 255);
-        rgblight_mode_noeeprom(1);
+        // Uncomment the following 2 lines and the "ledIndex" lines above to 
+        // change colors as the encoder turns.
+        // rgblight_sethsv_noeeprom((255/RGB_STEPS)*ledIndex, 255, 255);
+        // rgblight_mode_noeeprom(1);
     }
 }
 
+void matrix_scan_user(void) {
+    if(scrollMode) {
+        if(timer_elapsed(scrollTimer) < SCROLL_HOLD_TIME && scrollClicks > 1) {
+            dprint("switching scroll direction\n");
+            scrollHoriz = !scrollHoriz;
+            scrollClicks = 0;
+            scrollTimer = 0;
+            rgblight_sethsv_noeeprom(
+                scrollHoriz ? H_SCROLL_COLOR : V_SCROLL_COLOR, 
+                255, 
+                255
+            );
+        } else if(timer_elapsed(scrollTimer) > SCROLL_HOLD_TIME && scrollClicks == 1) {
+            dprint("leaving scroll mode\n");
+            rgblight_sethsv_noeeprom(VOL_COLOR, 255, 255);
+            scrollMode = 0;
+        }
+    }
+}
